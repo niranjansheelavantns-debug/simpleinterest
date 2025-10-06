fe45690dd118c204857cbdd1eddd6b780a4ce3c5
Niranjan  sheelavant <niranjansheelavant.ns@gmail.com>
2025-10-06 15:39:53 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -3,3 +3,9 @@ r=float(input("enter rate of interest:"))
 t=int(input("enter time period:"))
 si=(p*r*t)/100
 print("simple interest is :",si)
+
+ci= p* ((1 + r/ 100) ** t) -p
+print("compund interest is :",ci)
+
+
+
